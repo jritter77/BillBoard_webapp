c0ceee6fa11da5b97557c26547b9c3def2845a85
Jake Ritter <jakeritter77@gmail.com>
2022-01-11 14:32:08 -0800

    - fixing error

--- server/db/bill/getActiveBills.php
+++ server/db/bill/getActiveBills.php
@@ -7,11 +7,14 @@ $req = json_decode($_GET['req']);
 
 // sqlite3 command to be executed
 $stmt = $db->prepare("SELECT * FROM Bill WHERE user_id = :user_id AND (archived != 1 OR archived is NULL) 
-                      order by bill_year_due, bill_month_due, bill_day_due");
+                      order by bill_year_due, bill_month_due, bill_day_due
+                      limit :limit
+                      offset :offset");
 
 // fill in parameters
 $stmt->bindValue(':user_id', $req->user_id);
-
+$stmt->bindValue(':limit', $req->limit);
+$stmt->bindValue(':offset', $req->offset);
 
 // Execute the sqlite3 command
 $result = $stmt->execute();
